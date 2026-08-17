51e5faf67ee0ef79c27b759a8824ddb70b0d84a3
Ashish Gangwar <ashishgangwar009@gmail.com>
2026-08-17 17:10:53 +0530

    Fix merge conflicts in package-lock.json and update vite.config.js formatting

--- TechTorch-version2/Frontend/vite.config.js
+++ TechTorch-version2/Frontend/vite.config.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
->>>>>>> af41c8bc05a88454be791514e8727b222caaeb0b
 
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(),tailwindcss()],
 })
-=======
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  css: {
-    postcss: {},
-  },
-});
->>>>>>> af41c8bc05a88454be791514e8727b222caaeb0b
